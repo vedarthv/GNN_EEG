442bc4a72a5f3112bcdc97ec9140dd5be394b478
Vedarth V <vedarth1@gmail.com>
2023-02-15 13:51:27 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project emulating a GCN model which uses EEG data to classify depression in individuals.',
+    author='Xavier Mootoo, Mahd Sardar, and Vedarth Vyas',
+    license='MIT',
+)
